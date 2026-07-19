a4229541ae0d6a315cb298508cb97ba82e62155d
Simon Yakovlev <kukugrina@gmail.com>
2026-07-20 01:50:24 +0200

    integrate Rollbar error tracking for production environment v4

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('create/', UserCreateView.as_view(), name='user_create'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
